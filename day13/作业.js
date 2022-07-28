25efea65503c1d7d96a2eab2c36eebdb62701c15
PrePan <1367544416@qq.com>
2022-07-28 16:52:30 +0800

    day15

--- day13/作业.js
+++ day13/作业.js
@@ -1,4 +1,22 @@
 let toPay = document.querySelector('.toPay')
 toPay.addEventListener('click', function (){
-    alert('123')
+    document.querySelector('.mask').classList.add('visible')
+    document.querySelector('.pay').classList.remove('closePay')
+    document.querySelector('.pay').classList.add('openPay')
 })
+
+// 关闭支付界面
+function closePay(){
+    let pay = document.querySelector('.pay')
+    pay.classList.remove('closePay')
+    document.querySelector('.pay').classList.remove('openPay')
+    document.querySelector('.pay').classList.add('closePay')
+    setTimeout(() => {
+        document.querySelector('.mask').classList.remove('visible')
+    },500)
+}
+let close = document.querySelector('#close')
+let mask = document.querySelector('.mask')
+mask.addEventListener('click', closePay)
+close.addEventListener('click', closePay)
+
